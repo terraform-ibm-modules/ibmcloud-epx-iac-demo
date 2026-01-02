16a682ea7a43b47229fbaf0c65dd61fc764101af
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-02 20:21:34 +0000

    fix(deps): update dependencies

--- solutions/secure-bucket/main.tf
+++ solutions/secure-bucket/main.tf
@@ -9,7 +9,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.0"
+  version = "1.4.7"
   # could be input variable, or could be set here to force specific resource group
   existing_resource_group_name = "Default"
 }
@@ -19,7 +19,7 @@ module "resource_group" {
 #######################################################################################################################
 module "kms_key_crn_parser" {
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.3.0"
+  version = "1.3.7"
   crn     = var.existing_kms_key_crn
 }
 
@@ -43,7 +43,7 @@ locals {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.7.0"
+  version           = "10.8.3"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.bucket_name}-cos"
   cos_tags          = local.final_cos_tag_list
