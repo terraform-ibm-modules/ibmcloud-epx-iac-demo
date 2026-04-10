aadb832148cc09af13fbbb3bff208ec497249d09
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-04-10 19:22:01 +0000

    fix(deps): update dependencies

--- solutions/secure-bucket/main.tf
+++ solutions/secure-bucket/main.tf
@@ -9,7 +9,7 @@ locals {
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.5.0"
+  version = "1.6.0"
   # could be input variable, or could be set here to force specific resource group
   existing_resource_group_name = "Default"
 }
@@ -29,7 +29,7 @@ locals {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.14.10"
+  version           = "10.15.0"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.bucket_name}-cos"
   cos_tags          = local.final_cos_tag_list
