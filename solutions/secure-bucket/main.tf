26a44023e6f46aace2fc0367e20a325c6f9a94a3
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-05-15 19:20:07 +0000

    fix(deps): update dependencies

--- solutions/secure-bucket/main.tf
+++ solutions/secure-bucket/main.tf
@@ -29,7 +29,7 @@ locals {
 
 module "cos" {
   source            = "terraform-ibm-modules/cos/ibm"
-  version           = "10.15.2"
+  version           = "10.16.0"
   resource_group_id = module.resource_group.resource_group_id
   cos_instance_name = "${var.bucket_name}-cos"
   cos_tags          = local.final_cos_tag_list
